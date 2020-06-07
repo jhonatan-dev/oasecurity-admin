2deea8462bbbba1db122411fb903b5fdf9ec7a78
jhonatan-dev <jhonatancastillosalvador@hotmail.com>
2020-06-07 15:00:41 -0500

    favicon

--- index.js
+++ index.js
@@ -27,9 +27,8 @@ app.engine(
 );
 app.set("view engine", ".hbs");
 
-console.log(__dirname);
 //Middlewares
-//app.use(favicon(path.join(__dirname, "public/img", "logo.png")));
+app.use(favicon(path.join(__dirname, "/public/img", "logo.png")));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
